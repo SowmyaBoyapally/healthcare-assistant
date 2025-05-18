db440a6b0b139582d8c98ea7843749686c1dc61e
Sowmya Boyapally <boyapallysowmya@gmail.com>
2025-05-18 21:31:07 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,9 @@
+from transformers import MarianMTModel, MarianTokenizer
+
+def translate_to_english(text, src_lang='fr'):
+    tokenizer = MarianTokenizer.from_pretrained(f'Helsinki-NLP/opus-mt-{src_lang}-en')
+    model = MarianMTModel.from_pretrained(f'Helsinki-NLP/opus-mt-{src_lang}-en')
+    inputs = tokenizer(text, return_tensors="pt", padding=True)
+    translated = model.generate(**inputs)
+    return tokenizer.decode(translated[0], skip_special_tokens=True)
 
